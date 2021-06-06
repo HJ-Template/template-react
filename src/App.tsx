aaab042c6d9f3b9583d221b1e5deb17d71a9811c
as-you-say <dgf5820@naver.com>
2021-06-06 14:04:50 +0900

    Update router type

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { ReactElement } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 
 const repository = "template-react";
 
 export default function App(): ReactElement {
   return (
-    <BrowserRouter basename={repository}>
+    <HashRouter basename={repository}>
       <Route exact path="/" component={() => <>Home</>} />
       <Route path="/login" component={() => <>Login</>} />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
